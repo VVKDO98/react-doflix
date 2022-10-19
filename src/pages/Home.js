39520fcd866d404c4ba1a02d69f3151741a15136
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-19 13:15:10 +0200

    Add hero section and image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Hello</h1>
+            <div className='home__hero'>
+                <img src={process.env.PUBLIC_URL + "/assets/img/bg.jpg"} alt="Logo Netflix" />
+            </div>
         </div>
     );
 };
