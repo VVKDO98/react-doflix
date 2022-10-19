b561445349386b3f79bbeb0f7e4cd276df2e5162
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-19 12:11:09 +0200

    Draft menu css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Netflix from '../../public/assets/img/netflix.png'
 
 const Header = () => {
     return (
-        <div>
-            <img src={Netflix} alt="" />
+        <div className='header'>
+            <div className='header__block header__block--left'>
+                <img src={process.env.PUBLIC_URL + "/assets/img/menuicon.png"} alt="Burger menu" width="30px" />
+                <img src={process.env.PUBLIC_URL + "/assets/img/netflix.png"} alt="Logo Netflix" width="150px" />
+            </div>
+            <div className='header__block header__block--right'>
+                <img src={process.env.PUBLIC_URL + "/assets/img/searchicon.png"} alt="Logo Netflix" width="30px" />
+                <img src={process.env.PUBLIC_URL + "/assets/img/dotsicon.png"} alt="Logo Netflix" width="30px" />
+            </div>
         </div>
     );
 };
