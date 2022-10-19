e392e388329afd52731b9d5c05dcb567c566ecb0
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-19 14:24:03 +0200

    Changing gradient in header and adding hero content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,15 @@ const Home = () => {
     return (
         <div className='home'>
             <div className='home__hero'>
-                <img src={process.env.PUBLIC_URL + "/assets/img/bg.jpg"} alt="Logo Netflix" />
+                <div className='hero__content'>
+                    <img className='hero__image' src={process.env.PUBLIC_URL + "/assets/img/lucy.png"} alt="" width="400px" />
+                    <div className='hero__buttons'>
+                        <button className='hero__button'>Lecture</button>
+                        <button>Ma liste</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam numquam temporibus rerum fugiat esse consequatur quos optio exercitationem modi cumque!</p>
+                </div>
+                {/* <img className='home__background' src={process.env.PUBLIC_URL + "/assets/img/bg.jpg"} alt="Logo Netflix" /> */}
             </div>
         </div>
     );
