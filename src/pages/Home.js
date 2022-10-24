4ba175834ff8173025e315cfb81ed4ab9745694a
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-24 10:26:32 +0200

    Add some list and css of page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,13 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
     const API_POPULAR = "https://api.themoviedb.org/3/movie/popular?api_key=ef32645a58fd6506e28924ca1e4c975e&language=en-US&page=1";
-    const [movies, setMovies] = useState([]);
+    const API_TOPRATED = "https://api.themoviedb.org/3/movie/top_rated?api_key=ef32645a58fd6506e28924ca1e4c975e&language=en-US&page=1";
+    const API_TVSHOWS = "https://api.themoviedb.org/3/tv/popular?api_key=ef32645a58fd6506e28924ca1e4c975e&language=en-US&page=1";
+
+    const [popular, setPopular] = useState([]);
+    const [toprated, setTopRated] = useState([]);
+    const [tvshow, setTvShows] = useState([]);
+    
     const [width, setWidth] = useState(0);
     const carousel= useRef();
 
@@ -17,25 +23,55 @@ const Home = () => {
 
     useEffect(() => {
         axios.get(API_POPULAR)
-        .then((response) => setMovies(response.data.results));
+        .then((response) => setPopular(response.data.results));
+    }, []);
+
+    useEffect(() => {
+        axios.get(API_TOPRATED)
+        .then((response) => setTopRated(response.data.results));
+    }, []);
+
+    useEffect(() => {
+        axios.get(API_TVSHOWS)
+        .then((response) => setTvShows(response.data.results));
     }, []);
 
     // console.log(movies);
 
-    const renderMoviesList = () => (
-        movies.map((movie) => <MoviesItem key={movie.id} movie={movie} /> )
+    const renderMoviesList = (list) => (
+        list.map((movie) => <MoviesItem key={movie.id} movie={movie} /> )
     );
 
     return (
         <div className='home'>
             <Hero/>
             <div className='home__lists'>
-                <h2>Popular</h2>
-                <motion.div ref={carousel} className='main__carousel' whileTap={{cursor: "grabbing"}}>
-                    <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className='inner__carousel'>
-                        {renderMoviesList()}
-                    </motion.div>
-                </motion.div>
+                <div className="home__list">
+                    <div className="list__carousel">
+                        <h2>Popular</h2>
+                        <motion.div ref={carousel} className='main__carousel' whileTap={{cursor: "grabbing"}}>
+                            <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className='inner__carousel'>
+                                {renderMoviesList(popular)}
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                    <div className="list__carousel">
+                        <h2>Top Rated</h2>
+                        <motion.div ref={carousel} className='main__carousel' whileTap={{cursor: "grabbing"}}>
+                            <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className='inner__carousel'>
+                                {renderMoviesList(toprated)}
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                    <div className="list__carousel">
+                        <h2>Most popular series</h2>
+                        <motion.div ref={carousel} className='main__carousel' whileTap={{cursor: "grabbing"}}>
+                            <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className='inner__carousel'>
+                                {renderMoviesList(tvshow)}
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                </div>
             </div>
         </div>
     );
